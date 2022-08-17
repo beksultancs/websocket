dfd27d7bdf5c1ef3c33602d2b72a5952b771f91d
beksultan <beksultancs@gmail.com>
2022-08-18 00:44:46 +0600

    init commit

---
+++ src/main/java/websocket/WebController.java
@@ -0,0 +1,13 @@
+package websocket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+
+    @GetMapping("/")
+    String main() {
+        return "index";
+    }
+}
